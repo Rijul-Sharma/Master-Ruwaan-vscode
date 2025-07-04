388c022d21ce55f55833f89180bd9daec2dda0e9
Rijul-Sharma <rijul.sharma117@gmail.com>
2025-07-04 13:07:39 +0530

    context files feature added

--- extension.js
+++ extension.js
@@ -105,142 +105,147 @@ function activate(context) {
 			async message => {
 				if (message.type === 'prompt') {
 					const userPrompt = message.value;
+					const contextFiles = message.contextFiles;
+
+					let fullPrompt = '';
+
+					if (contextFiles && contextFiles.length > 0) {
+						fullPrompt += `Here are some relevant files for context:\n\n`;
+						for (const file of contextFiles) {
+							fullPrompt += `File: ${file.path}\n\`\`\`\n${file.content}\n\`\`\`\n\n`;
+						}
+					}
+
+					fullPrompt += userPrompt;
 
 					// 🧠 Add user message to chat history
 					// chatHistory.push({ role: 'user', content: userPrompt });
 
-					chatMessages.push({ role: 'user', content: userPrompt});
+					chatMessages.push({ role: 'user', content: fullPrompt});
 					context.workspaceState.update(CHAT_KEY, chatMessages);
+					await sendPrompt(chatMessages);
+				}
+
+				if (message.type === 'action') {
+					let editor = vscode.window.activeTextEditor;
+					let selection = editor?.selection;
+					let selectedText = editor?.document.getText(selection);
+					const hasSelectedCode = !!selectedText?.trim();
+					const hasContextFiles = !!message.contextFiles?.length;
+
+					if (!hasSelectedCode && !hasContextFiles) {
+						vscode.window.showWarningMessage('Please select some code or provide context files.');
+						return;
+					}
+
+					const extToLang = {
+						js: 'javascript', jsx: 'javascript', ts: 'typescript', tsx: 'typescript',
+						py: 'python', java: 'java', c: 'c', cpp: 'cpp', cc: 'cpp', cs: 'csharp',
+						go: 'go', rb: 'ruby', php: 'php', html: 'html', css: 'css', scss: 'scss',
+						json: 'json', xml: 'xml', sh: 'bash', bash: 'bash', rs: 'rust', kt: 'kotlin',
+						swift: 'swift', R: 'r', pl: 'perl', perl: 'perl', m: 'matlab', sql: 'sql',
+						dart: 'dart', scala: 'scala', vb: 'vbnet', groovy: 'groovy', jl: 'julia',
+						hs: 'haskell', erl: 'erlang', ex: 'elixir', exs: 'elixir', clj: 'clojure',
+						cljs: 'clojure', coffee: 'coffeescript', bat: 'bat', ps1: 'powershell',
+						psm1: 'powershell', fs: 'fsharp', fsx: 'fsharp', ada: 'ada', asm: 'assembly',
+						lua: 'lua', md: 'markdown', yml: 'yaml', yaml: 'yaml', toml: 'toml',
+						ini: 'ini', conf: 'conf', cfg: 'conf', tex: 'latex', latex: 'latex',
+						proto: 'protobuf', dockerfile: 'dockerfile', makefile: 'makefile',
+						cmake: 'cmake', vim: 'vim', vimrc: 'vim', zsh: 'zsh', fish: 'fish',
+						awk: 'awk', sed: 'sed', tcl: 'tcl'
+					};
+
+					// Language detection based on file extension
+					let language = '';
+					if (editor?.document?.fileName) {
+						const ext = editor.document.fileName.split('.').pop().toLowerCase() || '';
+						language = extToLang[ext] || '';
+					}
+
+					let actionPrompt = '';
+					let displayMessage = '';
+					let fullPrompt = '';
+
+					if (hasSelectedCode) {
+						const codeBlock = language
+							? `\n\n\`\`\`${language}\n${selectedText}\n\`\`\``
+							: `\n\n\`\`\`\n${selectedText}\n\`\`\``;
+
+							switch (message.value) {
+								case 'fix':
+									actionPrompt = `Fix this code:${codeBlock}`;
+									break;
+								case 'explain':
+									actionPrompt = `Explain what this code does, step by step:${codeBlock}`;
+									break;
+								case 'refactor':
+									actionPrompt = `Refactor the following code to improve structure, readability, or efficiency:${codeBlock}`;
+									break;
+								case 'comment':
+									actionPrompt = `Add helpful comments to this code to explain what it does:${codeBlock}`;
+									break;
+								default:
+									vscode.window.showWarningMessage('Unknown action.');
+									return;
+							}
+
+						fullPrompt = actionPrompt;
+						displayMessage = actionPrompt;
+					}
+
+					else if (hasContextFiles) {
+						const fileSnippets = [];
+						for (const file of message.contextFiles) {
+							const fileUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, file.path);
+							const contentBytes = await vscode.workspace.fs.readFile(fileUri);
+							const fileContent = Buffer.from(contentBytes).toString('utf8');
+							const ext = file.path.split('.').pop().toLowerCase();
+							const lang = extToLang[ext] || '';
+							fileSnippets.push(`\n\n---\nFile: ${file.path}\n\`\`\`${lang}\n${fileContent}\n\`\`\``);
+						}
+
+						const capitalized = message.value.charAt(0).toUpperCase() + message.value.slice(1);
+						actionPrompt = `${capitalized} the code in the following file(s).`;
+						fullPrompt = `${actionPrompt}\n\nThe following files might be relevant:\n${fileSnippets.join('\n')}`;
+
+						const fileNames = message.contextFiles.map(f => f.path.split('/').pop()).slice(0, 3);
+						const more = message.contextFiles.length > 3 ? ', ...' : '';
+						const fileList = `${fileNames.join(', ')}${more}`;
+						displayMessage = `${message.value.toUpperCase()} with the context of the following files: ${fileList}`;
+					}
+
+					chatMessages.push({ role: 'user', content: fullPrompt });
+					context.workspaceState.update(CHAT_KEY, chatMessages);
+
+					panel.webview.postMessage({
+						type: 'response',
+						value: displayMessage,
+						role: 'user'
+					});
 					await sendPrompt(chatMessages);
 
 				}
 
-				if (message.type === 'action') {
-					// const editor = vscode.window.activeTextEditor;
-					// if (!editor) {
-					// 	vscode.window.showErrorMessage('No active editor window found.');
-					// 	return;
-					// }
-					let editor = vscode.window.activeTextEditor;
-					let selection = editor?.selection;
-					let selectedText = editor?.document.getText(selection);
+				if (message.type === 'request-workspace-files') {
+					const files = await getWorkspaceFiles();
+					panel.webview.postMessage({ type: 'workspace-files', files});
+				}
 
-					// Later, inside the handler:
-					if (!editor || !selectedText?.trim()) {
-						vscode.window.showWarningMessage('Please select some code to fix.');
-						return;
-					}
+				if (message.type === 'request-file-content') {
+					const results = [];
 
-					// const selectedText = editor.document.getText(editor.selection);
-					if (!selectedText.trim()) {
-						vscode.window.showWarningMessage('Please select some code to fix.');
-						return;
+					for (const filePath of message.files) {
+						const fileUri = vscode.Uri.joinPath(vscode.workspace.workspaceFolders[0].uri, filePath);
+						const contentBytes = await vscode.workspace.fs.readFile(fileUri);
+						results.push({
+							path: filePath,
+							content: Buffer.from(contentBytes).toString('utf8'),
+						})
 					}
 
-					// Language detection based on file extension
-					let language = '';
-					if (editor && editor.document && editor.document.fileName) {
-						const ext = editor.document.fileName.split('.').pop().toLowerCase();
-						const extToLang = {
-							'js': 'javascript',
-							'jsx': 'javascript',
-							'ts': 'typescript',
-							'tsx': 'typescript',
-							'py': 'python',
-							'java': 'java',
-							'c': 'c',
-							'cpp': 'cpp',
-							'cc': 'cpp',
-							'cs': 'csharp',
-							'go': 'go',
-							'rb': 'ruby',
-							'php': 'php',
-							'html': 'html',
-							'css': 'css',
-							'scss': 'scss',
-							'json': 'json',
-							'xml': 'xml',
-							'sh': 'bash',
-							'bash': 'bash',
-							'rs': 'rust',
-							'kt': 'kotlin',
-							'swift': 'swift',
-							'R': 'r',
-							'pl': 'perl',
-							'perl': 'perl',
-							'm': 'matlab',
-							'sql': 'sql',
-							'dart': 'dart',
-							'scala': 'scala',
-							'vb': 'vbnet',
-							'groovy': 'groovy',
-							'jl': 'julia',
-							'hs': 'haskell',
-							'erl': 'erlang',
-							'ex': 'elixir',
-							'exs': 'elixir',
-							'clj': 'clojure',
-							'cljs': 'clojure',
-							'coffee': 'coffeescript',
-							'bat': 'bat',
-							'ps1': 'powershell',
-							'psm1': 'powershell',
-							'fs': 'fsharp',
-							'fsx': 'fsharp',
-							'ada': 'ada',
-							'asm': 'assembly',
-							'lua': 'lua',
-							'md': 'markdown',
-							'yml': 'yaml',
-							'yaml': 'yaml',
-							'toml': 'toml',
-							'ini': 'ini',
-							'conf': 'conf',
-							'cfg': 'conf',
-							'tex': 'latex',
-							'latex': 'latex',
-							'proto': 'protobuf',
-							'dockerfile': 'dockerfile',
-							'makefile': 'makefile',
-							'cmake': 'cmake',
-							'vim': 'vim',
-							'vimrc': 'vim',
-							'zsh': 'zsh',
-							'fish': 'fish',
-							'awk': 'awk',
-							'sed': 'sed',
-							'tcl': 'tcl'
-						};
-						language = extToLang[ext] || '';
-					}
+					// console.log(results, 'ye hain results');
 
-					const codeBlock = language ? `\n\n\`\`\`${language}\n${selectedText}\n\`\`\`` : `\n\n\`\`\`\n${selectedText}\n\`\`\``;
-					let actionPrompt = '';
-					if (message.value === 'fix') {
-						actionPrompt = `Fix this code:${codeBlock}`;
-					}
-					else if (message.value === 'explain') {
-						actionPrompt = `Explain what this code does, step by step:${codeBlock}`
-					}
-					else if (message.value === 'refactor') {
-						actionPrompt = `Refactor the following code to improve structure, readability, or efficiency:${codeBlock}`;
-					}
-					else if (message.value === 'comment') {
-						actionPrompt = `Add helpful comments to this code to explain what it does:${codeBlock}`;
-					}
-					else {
-						vscode.window.showWarningMessage('Unknown action.');
-						return;
-					}
-
-					chatMessages.push({ role: 'user', content: actionPrompt });
-					context.workspaceState.update(CHAT_KEY, chatMessages);
-					panel.webview.postMessage({
-						type: 'response',
-						value: actionPrompt,
-						role: 'user'
-					});
-					await sendPrompt(chatMessages);
+					panel.webview.postMessage({ type : 'context-files', files: results});
 				}
 			},
 			undefined,
@@ -266,6 +271,14 @@ function activate(context) {
 		html = html.replace('{{headerBg}}', headerBgUri.toString());
 
 		return html
+	}
+
+	async function getWorkspaceFiles() {
+		const include = '**/*.{js,ts,py,cpp,c,java,json,txt,md}';
+		const exclude = '**/node_modules/**';
+
+		const files = await vscode.workspace.findFiles(include, exclude);
+		return files.map(f => vscode.workspace.asRelativePath(f));
 	}
 	
 
